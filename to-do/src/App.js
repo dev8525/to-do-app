3f3ca5e93e8b3c2db62516873d9b96b6314ef525
dev8525 <kalashmali39@gmail.com>
2020-09-27 08:10:28 +0530

    created new react project and worked on app file and header component

---
+++ to-do/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from './components/header/header.component';
+
+const App= () => (
+    <div>
+        <Header />
+    </div>
+)
+
+export default App;
---
+++ to-do/src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => (
+    <div className='logo-box'>
+        <h1 className='heading-1'>To-Do</h1>
+    </div>
+);
+
+export default Header;
